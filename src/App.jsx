32d1151d47fceaa2f358091f5af6e3029dbf7bfe
Shanu S <shanuflash@gmail.com>
2023-03-13 14:45:36 +0530

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
+import { GiFarmer } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
-      <div>Vertical-Farm</div>
+      <div className="nav">
+        <div className="logo">
+          <GiFarmer className="logo-icon" />
+          Vertical Farm
+        </div>
+        <div className="nav-content">
+          <div className="nav-item nav-item-active">Solution</div>
+          <div className="nav-item">Department</div>
+          <div className="nav-item">Farming Method</div>
+          <div className="nav-item">Company</div>
+          <div className="nav-item">About</div>
+        </div>
+        <div className="nav-buttons">
+          <div className="button trial">Start Free Trial</div>
+          <div className="button account">Account</div>
+        </div>
+      </div>
     </div>
   );
 }
